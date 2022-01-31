491a72a6ed280ac392bbc881fcf5ffe3ccf8ab42
Textspot <sachinkatarki1995@gmail.com>
2022-01-31 19:38:39 +0530

    Added:Welcome to the Registration Programme

---
+++ src/test/java/com/bridgelabz/userregistration/UserRegistrationTest.java
@@ -0,0 +1,23 @@
+package com.bridgelabz.userregistration;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class UserRegistrationTest {
+
+	UserRegistration userRegistration = new UserRegistration();
+
+	@Test
+	public void givenFirstName_WhenProper_ShouldReturnTrue() {
+		boolean result = userRegistration.firstName("Sachin");
+		Assert.assertEquals(true, result);
+	}
+
+	@Test
+	public void givenFirstName_WhenNotProper_ShouldReturnFlase() {
+		boolean result = userRegistration.firstName("saCHIN");
+		Assert.assertEquals(false, result);
+	}
+
+}
