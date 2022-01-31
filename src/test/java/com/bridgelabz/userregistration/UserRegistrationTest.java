3e5e44f7f2ab382e22e777a60f729966d0ac156b
Textspot <sachinkatarki1995@gmail.com>
2022-01-31 20:09:12 +0530

    Added:Programme to Pre define Password Rule4

--- src/test/java/com/bridgelabz/userregistration/UserRegistrationTest.java
+++ src/test/java/com/bridgelabz/userregistration/UserRegistrationTest.java
@@ -92,4 +92,16 @@ public class UserRegistrationTest {
 		Assert.assertEquals(false, result);
 	}
 
+	@Test
+	public void givenPasswordRule4_WhenProper_ShouldReturnTrue() {
+		boolean result = userRegistration.passwordRule4("sachin@2022");
+		Assert.assertEquals(true, result);
+	}
+
+	@Test
+	public void givenPasswordRule4_WhenNotProper_ShouldReturnFalse() {
+		boolean result = userRegistration.passwordRule4("SaC22");
+		Assert.assertEquals(false, result);
+	}
+
 }
